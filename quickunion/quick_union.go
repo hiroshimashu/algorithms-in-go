d8c175d3566c718540c73da0a13458026bf3d529
hiroshimashu <hiroshimashu@gmail.com>
2022-02-14 22:38:27 +0900

    add: basic interface for quick union

---
+++ quickunion/quick_union.go
@@ -0,0 +1,17 @@
+package quickunion
+
+type QuickUnion struct {
+	ids[] int
+}
+
+func New(n int) *QuickUnion {
+	return &QuickUnion{}
+}
+
+func (qu *QuickUnion) Find(p, q int) bool {
+	return false
+}
+
+func (qu *QuickUnion) Union(p, q int) {
+
+}
